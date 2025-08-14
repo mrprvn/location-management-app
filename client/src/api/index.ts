2407b4d06fbb7f1a595d662ee14c915dccb20320
Praveen Singh <pvnsingh05@gmail.com>
2025-08-14 14:05:33 +0530

    fix env variable name

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
